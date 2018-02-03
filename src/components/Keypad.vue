fe4d7b03f5d60a5655b9dfe0592eebe46b3e7f05
Obinna Agba <obinna@mobnia.com>
2018-02-03 13:53:16 +0100

    Using v-for for rendering keypad component; using v-bind to attach keypad key value to data-key attribute of the keypad button

--- src/components/Keypad.vue
+++ src/components/Keypad.vue
@@ -1,81 +1,8 @@
 <template>
   <div class="key-box">
-    <div class="row key-row">
-      <div class="columns three">
-        <button v-on:click="keyClicked">Ans</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked">Del</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked" class="fsem15">(</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked" class="fsem15">)</button>
-      </div>
-    </div>
-    <div class="row key-row">
-      <div class="columns three">
-        <button v-on:click="keyClicked">7</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked">8</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked">9</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked" class="fsem15">+</button>
-      </div>
-    </div>
-    <div class="row key-row">
-      <div class="columns three">
-        <button v-on:click="keyClicked">4</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked">5</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked">6</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked" class="fsem15">-</button>
-      </div>
-    </div>
-    <div class="row key-row">
-      <div class="columns three">
-        <button v-on:click="keyClicked">1</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked">2</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked">3</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked" class="fsem15">x</button>
-      </div>
-    </div>
-    <div class="row key-row">
-      <div class="columns three">
-        <button v-on:click="keyClicked" class="fsem2">.</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked">0</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked" class="fsem15">=</button>
-      </div>
-      <div class="columns three">
-        <button v-on:click="keyClicked" class="fsem15">/</button>
-      </div>
-    </div>
-    <div class="row key-row">
-      <div class="columns six">
-        <button v-on:click="keyClicked" class="u-full-width">CLR</button>
-      </div>
-      <div class="columns six">
-        <button v-on:click="keyClicked" class="u-full-width pink">RESET</button>
+    <div class="row key-row" v-for="item in keys">
+      <div class="columns three" v-for="btn in item">
+        <button v-on:click="keyClicked" v-bind:class="btn.class" v-bind:data-key="btn.key">{{ btn.key }}</button>
       </div>
     </div>
   </div>
@@ -86,9 +13,36 @@
   export default Vue.component('keypad', {
     name: 'Keypad',
 
+    data: function () {
+      let keys = [
+        [
+          {key: 'Ans', class:""},{key: 'Del', class:""},{key: '(', class:"fsem15"},{key: ')', class:"fsem15"}
+        ],
+        [
+          {key: '7', class:""},{key: '8', class:""},{key: '9', class:""},{key: '+', class:"fsem15"}
+        ],
+        [
+          {key: '4', class:""},{key: '5', class:""},{key: '6', class:""},{key: '-', class:"fsem15"}
+        ],
+        [
+          {key: '1', class:""},{key: '2', class:""},{key: '3', class:""},{key: 'x', class:"fsem15"}
+        ],
+        [
+          {key: '.', class:"fsem2"},{key: '0', class:""},{key: '=', class:"fsem15"},{key: '/', class:"fsem15"}
+        ],
+        [
+          {key: 'CLR', class:"u-full-width"},{key: 'RESET', class:"u-full-width"}
+        ]
+      ];
+
+      return {
+        keys: keys
+      }
+    },
+
     methods: {
       keyClicked: function (event) {
-        const value = event.target.innerText;
+        const value = event.target.dataset.key;
         this.$emit('keypressed', value);
       }
     }
